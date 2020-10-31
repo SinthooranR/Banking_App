5bbb3ccdacf5408855f7c1b08eb92c3b5d43a3bc
Ibrahim <ibrahimabubaker55@gmail.com>
2020-10-30 20:18:57 -0400

    Minor CSS changes to welcome page

--- frontend/src/pages/Welcome/Welcome.js
+++ frontend/src/pages/Welcome/Welcome.js
@@ -2,16 +2,19 @@ import React from "react";
 import Typical from "react-typical";
 import classes from "./Welcome.module.css";
 import CreditCard from "../../components/CreditCards/CreditCard";
+import WelcomeGraph from "../../assets/welcomepagegraph.png";
 
 const Welcome = () => (
+  
   <div className={classes.Welcome}>
     <Typical
       steps={["Welcome", 3000, "Still in Development", 5000]}
       loop={Infinity}
       wrapper="h1"
     />
+    <h3 className={classes.SignUp}>Select from a variety of low interest option plans today.</h3>
     <span>
-      <h2 className={classes.SignUp}>Sign up for a credit card today!</h2>
+    <img className={classes.WelcomeImage} src={WelcomeGraph} />
       <CreditCard
         bankName="TD Canada Trust"
         cardNumber="5555 5555 5555 5555"
